9ec66b5cb38e66d87368299e054ca8a79bf9ba38
Jjooaogab <santosjoaogabriel09@gmail.com>
2023-12-15 23:19:06 -0300

    Change .env configs

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from "@prisma/client"
 import fastify from "fastify"
 import cors from '@fastify/cors'
 import { MessageRoutes, orderRoutes, productRoutes, userRoutes } from "./routes"
+import dotenv from 'dotenv'
 
 const app = fastify() // Criação do servidor, o servidor começa aqui ( Http ).
 
@@ -9,14 +10,19 @@ app.register(cors, {
   origin: true
 })
 
+dotenv.config()
+
 app.register(productRoutes)
 app.register(orderRoutes)
 app.register(userRoutes)
 app.register(MessageRoutes)
 
+const port = process.env.PORT || '3333'
+const portInt = parseInt(port)
+
 app.listen({ // Isso aqui serve para fazer o servidor ouvir uma 'URL'
-  port: 3333,
+  port: portInt,
 }).then((_res) => {
-  console.log('Servidor HTTP rodando em: http://localhost:3333/')
+  console.log(`Servidor HTTP rodando em: http://localhost:${port}/`)
 })
 
